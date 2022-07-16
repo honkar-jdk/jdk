71e99890a2c7d035d392eda4831214692d843921
Harshitha Onkar <harshitha.onkar@oracle.com>
2022-07-15 17:34:36 -0700

    initial commit BtnActionTest

--- test/jdk/java/awt/KeyboardFocusmanager/TypeAhead/ButtonActionKeyTest/ButtonActionKeyTest.java
+++ test/jdk/java/awt/KeyboardFocusmanager/TypeAhead/ButtonActionKeyTest/ButtonActionKeyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,46 +21,81 @@
  * questions.
  */
 
-/*
-  @test
-  @key headful
-  @bug        6396785
-  @summary    Action key pressed on a button should be swallowed.
-  @library    ../../../regtesthelpers
-  @build      Util
-  @run        main ButtonActionKeyTest
-*/
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.Robot;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.concurrent.atomic.AtomicBoolean;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
 import test.java.awt.regtesthelpers.Util;
 
+/*
+ * @test
+ * @key headful
+ * @bug 6396785
+ * @summary Action key pressed on a button should be swallowed.
+ * @library  /java/awt/regtesthelpers
+ * @build  Util
+ * @run  main/othervm ButtonActionKeyTest
+ */
+
 public class ButtonActionKeyTest {
-    Robot robot;
-    JFrame frame = new JFrame("Frame");
-    JButton button = new JButton("button");
-    JTextField text = new JTextField("text");
-    AtomicBoolean gotEvent = new AtomicBoolean(false);
+    static Robot robot;
+    static JFrame frame = new JFrame("Frame");
+    static JButton button = new JButton("button");
+    static JTextField text = new JTextField("text");
+    static AtomicBoolean gotEvent = new AtomicBoolean(false);
 
-    public static void main(String[] args) {
-        ButtonActionKeyTest app = new ButtonActionKeyTest();
-        app.init();
-        app.start();
+    public static void main(String[] args) throws Exception {
+        try {
+            robot = new Robot();
+            SwingUtilities.invokeAndWait(ButtonActionKeyTest::createAndShowGUI);
+            robot.setAutoDelay(300);
+            robot.waitForIdle();
+
+            Util.clickOnComp(button, robot);
+            robot.waitForIdle();
+
+            SwingUtilities.invokeAndWait(()-> {
+                if (!button.isFocusOwner()) {
+                    throw new RuntimeException("Test error:" +
+                            " Button didn't gain focus.");
+                }
+            });
+
+            robot.keyPress(KeyEvent.VK_A);
+            robot.delay(50);
+            robot.keyRelease(KeyEvent.VK_A);
+            robot.waitForIdle();
+
+            if (Util.waitForCondition(gotEvent, 1000)) {
+                throw new RuntimeException("An action key" +
+                        " went into the text field!");
+            }
+            System.out.println("Test passed.");
+        }
+        finally {
+            if (frame != null) {
+                SwingUtilities.invokeAndWait(()-> frame.dispose());
+            }
+        }
     }
 
-    public void init() {
-        robot = Util.createRobot();
-    }
-
-    public void start() {
+    private static void createAndShowGUI() {
         frame.setLayout(new FlowLayout());
         frame.add(button);
         frame.add(text);
         frame.pack();
 
         button.getInputMap().put(KeyStroke.getKeyStroke("A"), "GO!");
+
         button.getActionMap().put("GO!", new AbstractAction() {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Action performed!");
@@ -69,41 +104,18 @@ public class ButtonActionKeyTest {
         });
 
         text.addKeyListener(new KeyAdapter() {
-                public void keyTyped(KeyEvent e) {
-                    if (e.getKeyChar() == 'a') {
-                        System.out.println(e.toString());
-                        synchronized (gotEvent) {
-                            gotEvent.set(true);
-                            gotEvent.notifyAll();
-                        }
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == 'a') {
+                    System.out.println(e.toString());
+                    synchronized (gotEvent) {
+                        gotEvent.set(true);
+                        gotEvent.notifyAll();
                     }
                 }
-            });
+            }
+        });
+
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        Util.waitForIdle(robot);
-
-        Util.clickOnComp(button, robot);
-        Util.waitForIdle(robot);
-
-        if (!button.isFocusOwner()) {
-            throw new Error("Test error: a button didn't gain focus.");
-        }
-
-        robot.keyPress(KeyEvent.VK_A);
-        robot.delay(20);
-        robot.keyRelease(KeyEvent.VK_A);
-
-        if (Util.waitForCondition(gotEvent, 2000)) {
-            throw new TestFailedException("an action key went into the text field!");
-        }
-
-        System.out.println("Test passed.");
-    }
-}
-
-class TestFailedException extends RuntimeException {
-    TestFailedException(String msg) {
-        super("Test failed: " + msg);
     }
 }
